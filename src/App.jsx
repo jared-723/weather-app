5c7f68a823c74d5efbf4ad3a31bfdb79d5d8bd29
jared-723 <jarwend723@gmail.com>
2023-08-09 00:39:34 +0000

    finishing

---
+++ src/App.jsx
@@ -0,0 +1,72 @@
+import axios from 'axios'
+import './App.css'
+import { useEffect, useState } from 'react'
+import Weather from './components/Weather'
+
+function App() {
+
+  const [weatherInfo, setWeatherInfo] = useState(null)
+
+  const bgChange = 
+    {
+      "01d": '/img/bg1.jfif',
+      "02d": '/img/bg2.jfif',
+      "03d": '/img/bg3.jfif',
+      "04d": '/img/bg4.jfif',
+      "09d": '/img/bg6.jfif',
+      "10d": '/img/bg6.jfif',
+      "11d": '/img/bg9.jfif',
+      "13d": '/img/bg7.jfif',
+      "50d": '/img/bg4.jfif',
+      "01n": '/img/bg1.jfif',
+      "02n": '/img/bg2.jfif',
+      "03n": '/img/bg3.jfif',
+      "04n": '/img/bg4.jfif',
+      "09n": '/img/bg6.jfif',
+      "10n": '/img/bg6.jfif',
+      "11n": '/img/bg9.jfif',
+      "13n": '/img/bg7.jfif',
+      "50n": '/img/bg4.jfif'
+    }
+
+  const handleBgChange = (bg, obj) => {
+    let wI= ""
+    for (const key in obj) {
+      if(key == bg){
+        wI = obj[key]
+      }
+    }
+    return wI
+  }
+
+  const bg1 = handleBgChange(weatherInfo?.weather[0].icon , bgChange)
+
+  const success = (pos) =>{
+    const lat = pos.coords.latitude;
+    const lon = pos.coords.longitude;
+
+    const API_KEY = "e6479d3ee8d41c6d00dc508e2ba4da2c"
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+
+  axios.get(url)
+  .then(({data}) =>setWeatherInfo(data))
+  .catch((err) => console.log(err))
+
+  }
+
+  useEffect(() =>{
+    navigator.geolocation.getCurrentPosition(success);
+    ;
+  }, [])
+
+
+  return (
+    <>
+    <main className={`min-h-screen bg-[url(${bg1})] text-white flex justify-center items-center pl-3 pr-3`}>
+      <Weather weatherInfo={weatherInfo}/>
+    </main>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Weather.jsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+
+const Weather = ({weatherInfo}) => {
+
+    const [celsiusTemp, setCelsiusTemp] = useState(true)
+
+    const celcius = (temp)=>{
+        return (temp - 273.15).toFixed(1);
+    }
+
+    const fahrenheit = (tempC) => {
+        return(((tempC -273.15) * 9/5) + 32).toFixed(1);
+    } 
+
+    const handleUnitTemp = () => {
+        setCelsiusTemp(!celsiusTemp)
+    }
+
+    const resultTempConversion = celsiusTemp ? celcius(weatherInfo?.main.temp) : fahrenheit (weatherInfo.main.temp)
+
+    return (
+        <section className="basis-[600px] flex flex-col items-center gap-4">
+
+            <h2 className="text-2xl font-bold">{weatherInfo?.name}</h2>
+
+            <section className="grid gap-4 sm:grid-cols-[auto_auto]">
+
+                {/* Seccion superior */}
+                <section className="bg-white/60 p-2 rounded-2xl grid grid-cols-2 items-center text-center">
+                    <h4 className="col-span-2">{weatherInfo?.weather[0].description}</h4>
+                    <span className="grid place-items-center text-[4rem]">{resultTempConversion}{celsiusTemp ? "C" : "F"}</span>
+                    <div>
+                        <img src= {`https://openweathermap.org/img/wn/${weatherInfo?.weather[0].icon}@4x.png`} alt="" />
+                    </div>
+                </section>
+
+                {/* Seccion inferior */}
+                <section className="bg-white/60 rounded-2xl grid grid-cols-3 place-items-center p-6 text-lg sm:grid-cols-1">
+                    <article className="flex items-center gap-2">
+                        <div className="w-[18px]">
+                            <img src="/img/wind.png" alt="" />
+                        </div>
+                        <span>{weatherInfo?.wind.speed} m/s</span>
+                    </article>
+
+                    <article className="flex items-center gap-2">
+                        <div className="w-[18px]">
+                            <img src="/img/humidity.png" alt="" />
+                        </div>
+                        <span>{weatherInfo?.main.humidity} %</span>
+                    </article>
+
+                    <article className="flex items-center gap-2">
+                        <div className="w-[18px]">
+                            <img src="/img/pressure.png" alt="" />
+                        </div>
+                        <span>{weatherInfo?.main.pressure} hPa</span>
+                    </article>
+                </section>
+
+                <button onClick={handleUnitTemp} className="mt-4 bg-violet-500 block m-auto px-6 py-2 rounded-lg sm:col-span-2">Cambiar a F</button>
+
+            </section>
+    
+        </section>
+    )
+}
+export default Weather
